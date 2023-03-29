4fc895566da8ebad7f1e6da02a8218ad3f80a9a1
Luiz Fernando Pinagé Coutinho <luizfernandoluck@hotmail.com>
2023-03-28 22:51:50 -0300

    Refactor basevalidator

--- src/app/validator/BaseValidator.ts
+++ src/app/validator/BaseValidator.ts
@@ -1,6 +1,15 @@
 import IBaseValidator from '../protocol/validator/IBaseValidator';
+import JwtToken from '../utils/JwtToken';
 
 export abstract class BaseValidator<TInput, TOutput> implements IBaseValidator<TInput, TOutput> {
 	abstract validator(input: TInput): TOutput;
-	protected abstract auth(input: string): boolean;
+	protected auth(input: string): boolean {
+		try {
+			JwtToken.verifyToken(input);
+			return true;
+		} catch (error) {
+			console.log('>>>>', error);
+			return false;
+		}
+	}
 }
--- src/app/validator/ListUserValidator.ts
+++ src/app/validator/ListUserValidator.ts
@@ -2,22 +2,12 @@ import { HttpRequest, HttpResponse } from '../dto';
 import UserErrors from '../errors/UserErrors';
 import { IListUserValidator } from '../protocol/validator/IListUserValidator';
 import StatusCode from '../status/StatusCode';
-import JwtToken from '../utils/JwtToken';
 import { BaseValidator } from './BaseValidator';
 
 export default class ListUserValidator
 	extends BaseValidator<HttpRequest<void>, HttpResponse<number>>
 	implements IListUserValidator
 {
-	protected auth(input: string): boolean {
-		try {
-			JwtToken.verifyToken(input);
-			return true;
-		} catch (error) {
-			console.log('>>>>', error);
-			return false;
-		}
-	}
 	validator(input: HttpRequest<void>): HttpResponse<number> {
 		if (!this.auth(input.headers.token)) {
 			return {
